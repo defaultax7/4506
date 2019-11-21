df14ce91868fdb4679dba5ab076ebfcb2652c4cb
yu-chun-kit <johnson37siumen@hotmail.com>
2019-11-22 01:29:22 +0800

    edit slider

--- js/sliderrange.js
+++ js/sliderrange.js
@@ -2,11 +2,12 @@
     $( "#slider-price" ).slider({
       range: true,
       min: 0,
-      max: 50,
-      values: [ 0, 50 ],
+      max: 30,
+      values: [ 0, 30 ],
       slide: function( event, ui ) {
         $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-        $("#amount2").val("$" + ui.values[ 1 ]);
+        $("#amount1").val(ui.values[ 0 ]);
+        $("#amount2").val(ui.values[ 1 ]);
       }
     });
     // $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
